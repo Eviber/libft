166d1ec1975f94768499e36c377675eafe32f7fa
Youva GAUDE <ygaude@student.42.fr>
2016-11-10 16:34:38 +0100

    second commit

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,23 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ygaude <ygaude@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/10 10:53:19 by ygaude            #+#    #+#             */
-/*   Updated: 2016/11/10 10:53:23 by ygaude           ###   ########.fr       */
+/*   Created: 2016/11/10 10:56:26 by ygaude            #+#    #+#             */
+/*   Updated: 2016/11/10 14:41:13 by ygaude           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strcmp(const char *s1, const char *s2)
+#include "libft.h"
+
+int		ft_atoi(const char *str)
 {
-	int	res;
-	int	i;
+	int		i;
+	int		res;
+	int		sign;
 
 	i = 0;
-	while (s1 && s2 && s1[i] && s2[i] && s1[i] == s2[i])
+	res = 0;
+	sign = 1;
+	while (str[i] <= ' ' && str[i])
 		i++;
-	res = s1[i] - s2[i];
+	if ()
+	if (str[i])
+	{
+		if (str[i] == '+' || str[i] == '-')
+		{
+			if (str[i] == '-')
+				sign = -1;
+			i++;
+		}
+		
+	}
 	return (res);
 }
--- libft.h
+++ libft.h
@@ -15,6 +15,7 @@
 
 # include <string.h>
 
+int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t size);
 void	*ft_memalloc(size_t size);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
@@ -25,12 +26,15 @@ void	ft_memdel(void **ap);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *ptr, int byte, size_t size);
 char	*ft_strcat(char *s1, char *s2);
+int	ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strdup(const char *s1);
 char	*ft_strlcat(char *s1, char *s2, size_t n);
 int		ft_strlen(const char *str);
 char	*ft_strncat(char *s1, char *s2, size_t n);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
-char	*ft_strstr(const char *big, const char *small);
+char			*ft_strnstr(const char *big, const char *small, size_t len);
+char			*ft_strstr(const char *big, const char *small);
 
 #endif
